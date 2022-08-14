79a94f68ab2e4e27039373d8ca433cdfa9f647fe
xiyou <494949543@qq.com>
2022-08-14 10:19:17 +0800

    完成 chart10-11-12

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart-1';
@@ -10,6 +10,9 @@ import {Chart6} from '../components/chart-6';
 import {Chart7} from '../components/chart-7';
 import {Chart9} from '../components/chart-9';
 import {Chart8} from '../components/chart-8';
+import {Chart10} from '../components/chart-10';
+import {Chart11} from '../components/chart-11';
+import {Chart12} from '../components/chart-12';
 
 
 export const Home = () => {
@@ -40,7 +43,24 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="bordered section5"></section>
+        <section className="section5">
+          <div className="bordered row1 案发类型">
+            <h2>案发类型统计</h2>
+            <div className="charts">
+              <Chart10/>
+              <Chart11/>
+            </div>
+          </div>
+          <div className="bordered row2 案发街道">
+            <h2>案发街道统计</h2>
+            <div className="charts">
+              <Chart12/>
+            </div>
+          </div>
+          <div className="bordered row3 作案手段">
+            <h2>作案手段分析</h2>
+          </div>
+        </section>
       </main>
       <footer>&copy; 幼儿园扛把子 2022-{year}</footer>
     </div>
