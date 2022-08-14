986627d53ac4c11eceb7eb737f13eb1d8e7fd3f5
xiyou <494949543@qq.com>
2022-08-14 10:37:38 +0800

    完成 chart13-14

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,6 +13,8 @@ import {Chart8} from '../components/chart-8';
 import {Chart10} from '../components/chart-10';
 import {Chart11} from '../components/chart-11';
 import {Chart12} from '../components/chart-12';
+import {Chart13} from '../components/chart-13';
+import {Chart14} from '../components/chart-14';
 
 
 export const Home = () => {
@@ -55,10 +57,12 @@ export const Home = () => {
             <h2>案发街道统计</h2>
             <div className="charts">
               <Chart12/>
+              <Chart13/>
             </div>
           </div>
           <div className="bordered row3 作案手段">
             <h2>作案手段分析</h2>
+            <Chart14/>
           </div>
         </section>
       </main>
